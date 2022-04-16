0c84e71cdcf09a2b56eb7ef474b5082292535978
Hieu Minh <nmhieu04091999@gmail.com>
2022-04-16 11:01:16 +0700

    feat: responsive

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,41 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.autoplay').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    centerMode: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    centerMode: false
+                }
+            },
+            {
+                breakpoint: 200,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: true
+
+                }
+            }
+        ]
     });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('.slider__auto').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
